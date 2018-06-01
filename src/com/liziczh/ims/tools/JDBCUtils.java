d25ae6379680aea1f66a59db6823c2224673de04
liziczh <liziczh@foxmail.com>
2018-06-01 17:00:22 +0800

    使用dbcp.properties文件获取DataSource

--- src/com/liziczh/ims/tools/JDBCUtils.java
+++ src/com/liziczh/ims/tools/JDBCUtils.java
@@ -4,34 +4,38 @@ package com.liziczh.ims.tools;
 import org.apache.commons.dbcp.BasicDataSource;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class JDBCUtils {
-	public static final String DRIVER_CLASS_NAME = "oracle.jdbc.driver.OracleDriver";
-	public static final String URL = "jdbc:oracle:thin:@WIN-1V0BJOHAUB9:1521:orcl";
-	public static final String USERNAME = "scott";
-	public static final String PASSWORD = "tiger";
-	
-	private static final int MAX_IDLE = 3;
-	private static final long MAX_WAIT = 5000;
-	private static final int MAX_ACTIVE = 5;
-	private static final int INITIAL_SIZE = 10;
-	
+
 	private static BasicDataSource dataSource = new BasicDataSource();
 	static {
-		dataSource.setDriverClassName(DRIVER_CLASS_NAME);
-		dataSource.setUrl(URL);
-		dataSource.setUsername(USERNAME);
-		dataSource.setPassword(PASSWORD);
-			
-		dataSource.setMaxActive(MAX_IDLE);
-		dataSource.setMaxWait(MAX_WAIT);
-		dataSource.setMaxActive(MAX_ACTIVE);
-		dataSource.setInitialSize(INITIAL_SIZE);
+		InputStream in = JDBCUtils.class.getClassLoader().getResourceAsStream("dbcp.properties");
+		Properties properties = new Properties();
+		try {
+			properties.load(in);
+			dataSource.setDriverClassName(properties.getProperty("driverClassName"));
+			dataSource.setUrl(properties.getProperty("url"));
+			dataSource.setUsername(properties.getProperty("username"));
+			dataSource.setPassword(properties.getProperty("password"));
+
+			dataSource.setMaxActive(Integer.parseInt(properties.getProperty("maxIdle")));
+			dataSource.setMaxWait(Long.parseLong(properties.getProperty("maxWait")));
+			dataSource.setMaxActive(Integer.parseInt(properties.getProperty("maxActive")));
+			dataSource.setInitialSize(Integer.parseInt(properties.getProperty("initialSize")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public static DataSource getDataSource() {
 		return dataSource;
 	}
+
+
 
 	public static String PagenationSql(String sql, int currentPage, int pageSize){
 		String pageSql = "select * from ( select \"temp\".*, ROWNUM \"rn\" from ("
